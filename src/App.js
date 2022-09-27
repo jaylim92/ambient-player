178b270bb27645b436cd334f428d8c1f471df5ad
jaylim92 <jaylim3272@gmail.com>
2022-09-27 23:56:48 +0900

    날씨 api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Player from "./Player";
+import bg from "./assets/humid-bg.png";
+import Weather from "./Weather";
+
 function App() {
-  return <div>"hello"</div>;
+  return (
+    <div>
+      <Weather />
+      <Player></Player>
+      <img src={bg} alt="humid-bg" style={{ width: "500px" }} />
+    </div>
+  );
 }
 
 export default App;
